1ec446ff7926651fe9bb1bc23462105a01a41d96
Erlend Klakegg Bergheim <erlend@klakegg.net>
2015-07-21 11:11:27 +0200

    Remove references to SDP and PEPPOL.

--- src/main/java/no/difi/asic/SignatureMethod.java
+++ src/main/java/no/difi/asic/SignatureMethod.java
@@ -1,26 +1,17 @@
 package no.difi.asic;
 
 public enum SignatureMethod {
-    CAdES(null, MessageDigestAlgorithm.SHA256),
-    XAdES(null, MessageDigestAlgorithm.SHA256),
-
-    PEPPOL_V1(CAdES, MessageDigestAlgorithm.SHA256),
-    SDP(XAdES, MessageDigestAlgorithm.SHA256),
+    CAdES(MessageDigestAlgorithm.SHA256),
+    XAdES(MessageDigestAlgorithm.SHA256),
     ;
 
-    private SignatureMethod realSignatureMethod;
     private MessageDigestAlgorithm messageDigestAlgorithm;
 
-    SignatureMethod(SignatureMethod realSignatureMethod, MessageDigestAlgorithm messageDigestAlgorithm) {
-        this.realSignatureMethod = realSignatureMethod;
+    SignatureMethod(MessageDigestAlgorithm messageDigestAlgorithm) {
         this.messageDigestAlgorithm = messageDigestAlgorithm;
     }
 
     public MessageDigestAlgorithm getMessageDigestAlgorithm() {
         return messageDigestAlgorithm;
-    }
-
-    public SignatureMethod getRealSignatureMethod() {
-        return realSignatureMethod == null ? this : realSignatureMethod;
     }
 }
